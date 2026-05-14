35ee2f615fa4199fe773dbc8be3d31dee1f48530
leonardoemvocacao <leonardosos30@gmail.com>
2026-05-14 16:01:27 -0300

    "Codigo-da-Transformacao-Python.py

---
+++ Leonardo_codigo_da_transformacao_EAD/modulo_2/ead.py
@@ -0,0 +1,14 @@
+n1 = float(input("Digite o primeiro número: "))
+n2 = float(input("Digite o segundo número: "))
+
+soma = n1 + n2
+subtracao = n1 - n2
+multiplicacao = n1 * n2
+divisao = n1 / n2
+resto = n1 % n2
+
+print(f"Soma: {soma}")
+print(f"Subtração: {subtracao}")
+print(f"Multiplicação: {multiplicacao}")
+print(f"Divisão: {divisao}")
+print(f"Resto da divisão: {resto}")
